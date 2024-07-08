784ec5cc0faa7a3b8137bd506779960e95fb2efb
Gideon Nwokpor <gideonjohnson70@gmail.com>
2024-07-08 21:55:03 +0100

    update

--- src/pages/main/proofreader/index.tsx
+++ src/pages/main/proofreader/index.tsx
@@ -153,12 +153,7 @@ function ProofReaderPage() {
           setQuery={setQuery}
           setSelected={setSelected}
         />
-        <div className="flex-1 py-6 px-8 rounded-[8px] flex items-center justify-center relative bg-white h-[300px] md:h-[80vh] font-montserrat">
-          {/* <h1 className="text-[18px] leading-[27px] font-semibold absolute top-6 left-8">
-          Streamline Lesson Planning with Automated Question Generation
-        </h1> */}
-          {/* {getProofReaderData?.data.length === 0 ? ( */}
-
+        <div className="flex-1 py-6 px-8 rounded-[8px] flex items-center justify-center relative bg-white h-[300px] md:h-[80vh] font-montserrat overflow-y-auto">
           <div className="w-full h-full min-h-[80vh] bg-white grid grid-cols-1 md:grid-cols-3 md:divide-x divide-[#EFEFEF]">
             <div className="md:col-span-2 p-5 md:py-10 h-full overflow-y-auto">
               {isSelected ? (
