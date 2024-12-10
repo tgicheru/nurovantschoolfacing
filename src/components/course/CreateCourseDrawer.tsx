63ace1b570bcfba5a26e96e5639567e440471548
Gideon Nwokpor <gideonjohnson70@gmail.com>
2024-12-10 16:40:33 +0100

    updates to bug fix

--- src/components/course/CreateCourseDrawer.tsx
+++ src/components/course/CreateCourseDrawer.tsx
@@ -96,7 +96,7 @@ const CreateCourseDrawer = ({
       accessKeyId: process.env["REACT_APP_AWS_ACCESS_KEY_ID"],
       secretAccessKey: process.env["REACT_APP_AWS_SECRET_ACCESS_KEY"],
       // region: process.env["REACT_APP_AWS_REGION"],
-      region: "us-east-1",
+      region: "us-east-2",
     });
 
     // Specify the bucket and key (object key) for the upload
